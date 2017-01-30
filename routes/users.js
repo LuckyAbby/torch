8564f396250c7fb1ac0c7855471433c3f10b854d
LuckyAbby <1370903525@qq.com>
2017-01-30 11:50:03 +0800

    修改路由

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/write', function(req, res, next) {
+  if(req.session.user) {
+    res.render('write');
+  }
+  else {
+    res.redirect('/');
+  }
 });
+
+router.post('/write', function(req, res, next) {
+  if(req.session.user) {
+  var article_title=req.body.art_title;
+  var article_type=req.body.type;
+  var article_content=req.body.textarea;
+  var student_id = req.session.user.student_id;
+  var article_time = new Date();
+  req.getConnection(function(errConn,connection) {
+    if(errConn) {
+      console.error('connection error: ', errConn);
+      return next(errConn);
+    }
+    var sql='insert into articles (article_title,article_type,article_content,student_id,article_time) VALUES(?,?,?,?,?)';
+    connection.query(sql,[article_title,article_type,article_content,student_id,article_time],function(errQuery, result) {
+      console.log('sql: ', sql);
+      if(errQuery) {
+        console.error('query error: ', errConn);
+        return next(errQuery);
+      }
+      console.log('result: ', result);
+      // var message=encodeURI('保存成功');
+    return res.render('write', {
+      message:'保存成功'
+    });
+  });
+});
+}
+  else {
+    res.redirect('/');
+  }
+});
+
 
 module.exports = router;
