deb13dcb1eb5cdbae31271ec757b70f0683343c6
LuckyAbby <1370903525@qq.com>
2017-02-04 19:43:22 +0800

    update database

--- database/torch.sql
+++ database/torch.sql
@@ -1,3 +1,52 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : torch
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2017-02-04 19:42:13
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for articles
+-- ----------------------------
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_title` varchar(255) DEFAULT NULL,
+  `article_content` text,
+  `article_clicks` int(11) DEFAULT '0',
+  `support_number` int(11) DEFAULT '0',
+  `oppose_number` int(11) DEFAULT '0',
+  `comments_number` int(11) DEFAULT '0',
+  `student_id` int(11) DEFAULT NULL,
+  `article_type` varchar(255) DEFAULT NULL,
+  `article_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`article_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_content` varchar(255) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `comment_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+
 -- ----------------------------
 -- Table structure for login
 -- ----------------------------
@@ -11,48 +60,19 @@ CREATE TABLE `login` (
   `class` varchar(255) DEFAULT NULL,
   `login_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`student_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf-8;
-
--- ----------------------------
--- Table structure for articles
--- ----------------------------
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE `articles` (
-  `article_id` int(11) NOT NULL,
-  `article_title` varchar(255) NOT NULL,
-  `article_content` text NOT NULL,
-  `article_clicks` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `article_type` varchar(255) NOT NULL,
-  `article_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`article_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf-8;
-
--- ----------------------------
--- Table structure for comments
--- ----------------------------
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-  `comment_id` int(11) NOT NULL,
-  `comment_content` varchar(255) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `article_id` int(11) NOT NULL,
-  `comment_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`comment_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf-8;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for oppose
 -- ----------------------------
 DROP TABLE IF EXISTS `oppose`;
 CREATE TABLE `oppose` (
-  `oppose_id` int(11) NOT NULL,
+  `oppose_id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` int(11) NOT NULL,
   `article_id` int(11) NOT NULL,
   `oppose_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`oppose_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf-8;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for support
@@ -64,4 +84,5 @@ CREATE TABLE `support` (
   `article_id` int(11) NOT NULL,
   `support_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`support_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf-8;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET FOREIGN_KEY_CHECKS=1;
