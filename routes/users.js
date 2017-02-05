403bbf6169f62f68cc26e1756b7c0a119bb282b5
LuckyAbby <1370903525@qq.com>
2017-02-05 22:45:35 +0800

    update support

--- routes/users.js
+++ routes/users.js
@@ -81,12 +81,56 @@ router.post('/comment',function(req,res) {
 
 router.post('/support',function(req,res,next) {
   if(!req.session.user) {
+    console.log('11111');
   res.json({
       code: 1001,
       message: '未登录',
     });
   }
-
+  var article_id=req.body.article_id;
+  var supportFlag=req.body.support;
+  var student_id=req.session.user.student_id;
+  var data = {
+    article_id,
+    student_id,
+    support_flag:supportFlag,
+    support_time:new Date(),
+  };
+  console.log("data is:",data);
+  req.getConnection(function(errConn,connection) {
+    if(errConn) {
+      console.error('connection error: ', errConn);
+      return next(errConn);
+    }
+    var supportSql='insert  into support set ?';
+    var cancelSupportSql='insert into support set ?';
+    if(supportFlag===true) {
+      console.log('2222222');
+      connection.query(supportSql,[data],function(errQuery,result) {
+        if(errQuery) {
+          console.error('query error: ', errQuery);
+          return next(errQuery);
+        }
+        return res.json({
+          code: 0,
+          message:'点赞成功',
+        })
+      })
+    }
+    else {
+      console.log('3333333333');
+      connection.query(cancelSupportSql,[data],function(eerrQuery,result) {
+        if(errQuery) {
+          console.error('errQuery',errQuery);
+          return next(errQuery);
+        }
+        return rej.json({
+          code:1,
+          message:'取消点赞成功',
+        })
+      })
+    }
+  })
 })
 
 module.exports = router;
